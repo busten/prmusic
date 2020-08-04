c7dfaae18288a65e5dd3e1106d9771125bc77be2
busten <2431279624@qq.com>
2020-08-04 22:16:03 +0800

    git push origin master

---
+++ src/views/view_home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+      <p>这是主页</p>
+      <span>默认图标<SvgIcon name="def"/></span>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,46 @@
+// const path = require('path')
+// function resolve(dir) {
+//  return path.join(__dirname, '.', dir)
+// }
+module.exports = {
+  devServer: {
+    open: true,
+    host: 'www.demo.com',
+    https: false,
+    port: 8081,
+    proxy: {
+      '/prmusic': {
+        target: 'http://www.demo.com:8080/prmusic',
+        ws: true,
+        changOrigin: true,
+        pathRewrite: {
+          '^/prmusic': '/'
+        }
+      }
+    }
+  },
+  // chainWebpack: config => {
+  //   config.module
+  //     .rule('svg')
+  //     .exclude.add(resolve('src/icons'))
+  //     .end()
+
+  //   config.module
+  //     .rule('icons')
+  //     .test(/\.svg$/)
+  //     .include.add(resolve('src/icons'))
+  //     .end()
+  //     .use('svg-sprite-loader')
+  //     .loader('svg-sprite-loader')
+  //     .options({
+  //       symbolId: 'icon-[name]'
+  //     })
+  // },
+  pluginOptions: {
+    svgLoader: {
+      svgo: {
+        plugins: []
+      }
+    }
+  }
+}
